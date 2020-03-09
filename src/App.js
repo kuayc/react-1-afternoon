24f012b53dede5433698cb3c479e691382971c51
Chin Kuay <kuaychin@gmail.com>
2020-03-09 16:26:49 -0500

    react first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./index.css";
+import TopicBrowser from "./components/TopicBrowser/TopicBrowser";
 
 class App extends Component {
   render() {
-    
+    return (
+      <div>
+        <h1>Hello</h1>
+        <TopicBrowser />
+      </div>
+    );
   }
 }
 
